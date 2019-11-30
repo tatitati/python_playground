f25b867fe0de05eabf24db1e4f4823f11c3de01a
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-11-30 22:54:26 +0000

    piepeline with threads properly done?

--- 2-Threading_with_Lock/Pipeline_with_queue.py
+++ 2-Threading_with_Lock/Pipeline_with_queue.py
@@ -1,46 +1,69 @@
 from queue import Queue
 import threading
 from time import sleep
+import re
 
-queue = Queue()
+queue1 = Queue()
+queue2 = Queue()
 
-def readFile():
+def readLineFile():
     with open('2-Threading_with_Lock/text.txt') as f:
         for line in f:
             yield line
 
-def stage1(func, toQueue):
-    print("Stage1: waiting")
-    result = queue.get()
+def findLinks(text):
+    return re.findall('https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', text)
+
+def stage1():
+    for line in readLineFile():
+        sleep(1)
+        print("Stage1: putting")
+        queue1.put(line)
     print("Stage1: done")
-    func(result)
 
-def stage2(func, fromQueue, toQueue):
-    print("Stage2: waiting")
-    result = queue.get()
-    print("Stage2: done")
-    func(result)
+def stage2():
+    while True:
+        textline = queue1.get()
+        sleep(1)
+        urls = findLinks(textline)
+        for url in urls:
+            print(f"\t\t\t\tstage2: Received -> {url}")
+            queue2.put(url)
+        print("\t\t\t\tStage2: done")
 
-def stage3(func, fromQueue):
-    print("Stage3: waiting")
-    result = queue.get()
-    print("Stage3: done")
-    func(result)
 
-def run_threads(functionTarget):
+def stage3():
+    while True:
+        url = queue2.get()
+        sleep(1)
+        print(f"\t\t\t\t\t\t\t\tstage3: Received -> {url}")
+        print("\t\t\t\t\t\t\t\tStage3: done")
+
+def run_threads():
+    t1 = threading.Thread(target=stage1)
+    t2 = threading.Thread(target=stage2)
+    t3 = threading.Thread(target=stage3)
+
+    t1.start()
+    t2.start()
+    t3.start()
+
     threads = []
-    t = threading.Thread(target=functionTarget)
-    t.start()
+    threads.append(t1)
+    threads.append(t2)
+    threads.append(t3)
 
+    for thread in threads:
+        thread.join()
 
+# iterator = readLineFile()
+# line1 = iterator.__next__()
+# line2 = iterator.__next__()
+# line3 = iterator.__next__()
+#
+# print(line1)
+# print(line2)
+# print(line3)
 
-iterator = readFile()
-line1 = iterator.__next__()
-line2 = iterator.__next__()
-line3 = iterator.__next__()
-
-print(line1)
-print(line2)
-print(line3)
-
+run_threads()
 
