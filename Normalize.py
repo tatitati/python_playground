1424e2ef2b8c1dd733171ba48ebb613ff08260af
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-11-30 14:37:31 +0000

    added an special case about iterators and yield

--- Normalize.py
+++ Normalize.py
@@ -1,26 +1,22 @@
 import re, string, sys
 
+def normalize(iterator):
+    total = sum(iterator())
 
-
-def normalize(numbers):
-    total = sum(numbers)
     result = []
-    for value in numbers:
+    for value in iterator():
         percent = 100*value/total
         result.append(percent)
 
     return result
 
 
-def read_visits(numbers):
+def get_iterator(numbers):
     for num in numbers:
         yield num
 
 if __name__ == '__main__':
     listNumbers = [15, 35, 80]
-    print(normalize(listNumbers)) # [11.538461538461538, 26.923076923076923, 61.53846153846154]
 
-
-    iterator = read_visits(listNumbers)
-    result = normalize(iterator)
-    print(result) # []
+    result = normalize(lambda: get_iterator(listNumbers))
+    print(result)
