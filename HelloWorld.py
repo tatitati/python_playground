37ec1c7f5f89cd19b93cf087a922eb0799efa73d
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-11-30 14:10:58 +0000

    more

--- HelloWorld.py
+++ HelloWorld.py
@@ -3,9 +3,11 @@ import re, string, sys
 
 def index_words(text):
     if text:
+        print("called--")
         yield 0
     for index, letter in enumerate(text):
         if letter == ' ':
+            print("called")
             yield index+1
 
 
@@ -13,16 +15,39 @@ if __name__ == '__main__':
     words = 'Four score and seven years ago'
 
     iterator1 = index_words(words)  # result: <generator object index_words at 0x106724c00>
-    print(list(iterator1))  # [0, 5, 11, 15, 21, 27]
+    print(list(iterator1))
+    # called--
+    # called
+    # called
+    # called
+    # called
+    # [0, 5, 11, 15, 21, 27]
+
+    print(list(iterator1))
+    # []
 
     iterator2 = index_words(words) # result: <generator object index_words at 0x106724c00>
-    print(iterator2.__next__()) # 0
-    print(iterator2.__next__()) # 5
-    print(iterator2.__next__()) # 11
-    print(iterator2.__next__()) # 15
-    print(iterator2.__next__()) # 21
-    print(iterator2.__next__()) # 27
-    print(iterator2.__next__()) # StopIteration
+    print(iterator2.__next__())
+    # called--
+    # 0
+    print(iterator2.__next__())
+    # called
+    # 5
+    print(iterator2.__next__())
+    # called
+    # 11
+    print(iterator2.__next__())
+    # called
+    # 15
+    print(iterator2.__next__())
+    # called
+    # 21
+    print(iterator2.__next__())
+    # called
+    # 27
+
+
+
 
 
 
